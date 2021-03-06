9798bf390f3865c5d80ef9b12e60574216e498f4
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-06 19:31:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,3 @@
-class Artist
-  attr_accessor :name, :songs
 
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @songs = []
-    save
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-  
-  def save
-    @@all << self
-  end
-  
-  def self.all
-    @all
-  end
-  
-end
   
   
