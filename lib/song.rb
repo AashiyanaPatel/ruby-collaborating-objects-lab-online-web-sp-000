f0ab3317cf0dae7797d552cf941c10424e871031
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-06 19:02:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,8 @@ class Song
   
   def self.new_by_filename(filename)
     split_name = filename.split(" - ")
+    song = self.new(plit_name[1])
+    artist = Artist.find_or_create_by_name(name)
   end
 
   def artist_name=(name)
