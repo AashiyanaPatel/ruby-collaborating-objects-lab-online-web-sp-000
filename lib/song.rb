96f591135c88ad437fd5e627db47b837cec4be79
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-06 19:10:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,4 @@ class Song
     artist.add_song(song)
     song
   end
-
-  def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
-    artist.add_song(self)
-  end
 end
