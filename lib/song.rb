d16ae4612b08d7faf981dc1c297eeb5c3bf74e96
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-06 19:00:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,12 +5,8 @@ class Song
     @name = name
   end
   
-  def self.new_by_filename(file)
-    artist_name = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
-    song.artist_name = artist_name
-    song
+  def self.new_by_filename(filename)
+    split_name = filename.split(" - ")
   end
 
   def artist_name=(name)
