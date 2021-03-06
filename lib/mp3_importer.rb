d5a0a2368910d16f1153b924f10c13a159d4ecd2
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-06 18:26:41 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,17 +1,3 @@
- class MP3Importer
-  attr_accessor :path
 
-  def initialize(path)
-    @path = path
-  end
-
-  def files
-    Dir.entries(@path).delete_if { |i| !i.end_with?("mp3") }
-  end
-
-  def import
-    files.each { |file| Song.new_by_filename(file) }
-  end
-end
 
  
